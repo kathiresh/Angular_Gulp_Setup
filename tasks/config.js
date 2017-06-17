d9867f5b1fd1039850315a581903e7eeff79f8b9
pravinkumar <pravinkumar@ideas2it.com>
2017-06-17 17:20:26 +0530

    Angualr gulp initial setup

---
+++ tasks/config.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var chalk = require( 'chalk' );
+
+/*-----------------------------------------------------------
+ GULP: APP CONFIGURATION
+ Source, Build folder and other application configuration
+-----------------------------------------------------------*/
+var config = function() {
+
+	// Source Path
+	var src = {
+		root: 'app',
+		cssPath: 'app/assets/css',
+		scssPath: 'app/assets/scss',
+		css: [
+			'app/assets/css/style.css',
+			'bower_components/toastr/toastr.min.css'
+		],
+		scss: [
+			'app/assets/scss/*.scss'
+		],
+		js: 'app/scripts',
+		images: 'app/assets/images',
+		fonts: 'app/assets/fonts',
+		bower: './bower_components',
+		libs: [
+			'bower_components/angular/angular.min.js',
+			'bower_components/angular-aria/angular-aria.min.js',
+			'bower_components/angular-animate/angular-animate.min.js',
+			'bower_components/angular-sanitize/angular-sanitize.min.js',
+			'bower_components/angular-mocks/angular-mocks.js',
+			'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+			'bower_components/jquery/dist/jquery.min.js',
+			'bower_components/toastr/toastr.min.js'
+		]
+	};
+
+	// Build Path
+	var build = {
+		root: 'build',
+		css: 'build/assets/css',
+		js: 'build/js',
+		images: 'build/assets/images',
+		fonts: 'build/assets/fonts',
+		filePath: {
+			js: [
+				'build/js/bower.js',
+				'build/js/application.js',
+				'build/js/templates.js'
+			],
+			minJs: [
+				'build/js/application.min.js',
+			]
+		}
+	};
+	//Server Configuration
+	var serverConfiguration = {
+		host: 'localhost',
+		port: 3001,
+		open: true,
+		livereload: {
+			enable: true,
+			port: 35729
+		}
+	};
+
+	//Default production mode set to false
+	var production = false;
+
+	//Bower Configuration
+	var bowerConfiguration = {
+		paths: {
+			bowerDirectory: src.bower,
+			bowerrc: '.bowerrc',
+			bowerJson: 'bower.json'
+		}
+	};
+
+	// Minification options for HTML
+	var opts = {
+		comments: false,
+		quotes: true,
+		spare: true,
+		empty: true,
+		cdata: true
+	};
+
+	// Chalk config
+	var notify = {
+		error: chalk.red.bold,
+		warning: chalk.black.bold.bgYellow,
+		update: chalk.yellow.underline,
+		success: chalk.green
+	};
+
+	// CSS autoprefix config
+	var browserVersion = [ 'last 2 versions' ];
+
+	return {
+		source: src,
+		build: build,
+		serverConfiguration: serverConfiguration,
+		production: production,
+		bowerConfiguration: bowerConfiguration,
+		opts: opts,
+		notify: notify,
+		browserVersion: browserVersion,
+		serverIP: '127.0.0.1'
+	};
+};
+
+module.exports = config();
---
+++ tasks/image.js
@@ -0,0 +1,23 @@
+/*===========================================================
+ GULP: APP TASKS :: Images minification
+===========================================================*/
+'use strict';
+
+var gulp = require( 'gulp' );
+var imagemin = require( 'gulp-imagemin' );
+var svgSprite = require( 'gulp-svg-sprites' );
+var config = require( './config' );
+
+//Image minification
+gulp.task( 'image', [ 'sprites' ], function() {
+	gulp.src( config.source.images + '/*' )
+		.pipe( imagemin() )
+		.pipe( gulp.dest( config.build.images ) );
+} );
+
+//SVG image sprites
+gulp.task( 'sprites', function() {
+	return gulp.src( [ config.source.images + '/*.svg', config.source.images + '/**/*.svg' ] )
+		.pipe( svgSprite() )
+		.pipe( gulp.dest( config.build.images + '/svg/' ) );
+} );
